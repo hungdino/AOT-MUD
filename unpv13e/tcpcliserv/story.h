214dcf23a327178370ffd451350b144d5c3e1ef0
Wei-Ting HUNG <wthung.cs07@nycu.edu.tw>
2023-12-14 15:32:58 +0800

    Define StoryNode

--- unpv13e/tcpcliserv/story.h
+++ unpv13e/tcpcliserv/story.h
@@ -20,12 +20,13 @@ typedef struct {
 
 // 故事節點結構
 typedef struct {
-    char story[STORY_LENGTH];
-    Character characters[MAX_CHARACTERS_PER_NODE];
-    int numberOfCharacters;
+    int nodeSeriesNum; // 節點編號
+    char story[STORY_LENGTH]; // 故事提示
+    Character characters[MAX_CHARACTERS_PER_NODE]; // 儲存角色和各自的選項
+    int numberOfCharacters; // 
 } StoryNode;
 
-
+/*
 // 全局變數
 StoryNode node1 = {
     "在艾連製造出的巨人骨架保護下，米卡莎與阿爾敏總算從托洛斯特區安然無恙的返回，艾連自己從巨人身體中走了出來，並表示他想起了父親曾告訴他所有事情只要回到家裡的地下室就會有所解答。",
@@ -41,12 +42,4 @@ StoryNode node1 = {
     },
     1  // 这个节点有1个角色
 };
-
-
-void displayStoryNode(StoryNode node) {
-    printf("故事: %s\n", node.story);
-    for (int i = 0; i < 3; i++) {
-        printf("選項 %d: %s\n", i + 1, node.options[i].description);
-    }
-}
-
+*/
