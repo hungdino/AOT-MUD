1c84c2f7fff46613af21613c6ff136a063a274a3
Wei-Ting HUNG <wthung.cs07@nycu.edu.tw>
2023-12-14 15:27:33 +0800

    Define struct

--- unpv13e/tcpcliserv/story.h
+++ unpv13e/tcpcliserv/story.h
@@ -1,30 +1,47 @@
-#include <stdio.h>
 
-// 定義選項的結構
+#define MAX_OPTIONS_PER_CHARACTER 3
+#define MAX_CHARACTERS_PER_NODE 3
+#define OPTION_DESC_LENGTH 256
+#define STORY_LENGTH 1024
+#define ID_LENGTH 32
+
+// 選項結構
 typedef struct {
-    char description[256];  // 選項描述
-    int nextNodeIndex;      // 根據這個選項導向的下一個節點的索引
+    char description[OPTION_DESC_LENGTH];
+    int nextNodeIndex;
 } Option;
 
-// 定義故事節點的結構
+// 定義角色結構
 typedef struct {
-    char story[1024];   // 故事內容
-    Option options[3];  // 每個節點有3個選項
+    char ID[ID_LENGTH];
+    Option options[MAX_OPTIONS_PER_CHARACTER];
+    int numberOfOptions;
+} Character;
+
+// 故事節點結構
+typedef struct {
+    char story[STORY_LENGTH];
+    Character characters[MAX_CHARACTERS_PER_NODE];
+    int numberOfCharacters;
 } StoryNode;
 
-// 函數原型聲明
-void displayStoryNode(StoryNode node);
 
-/*
+// 全局變數
 StoryNode node1 = {
-        "在艾連製造出的巨人骨架保護下，米卡莎與阿爾敏總算從托洛斯特區安然無恙的返回。",
+    "在艾連製造出的巨人骨架保護下，米卡莎與阿爾敏總算從托洛斯特區安然無恙的返回，艾連自己從巨人身體中走了出來，並表示他想起了父親曾告訴他所有事情只要回到家裡的地下室就會有所解答。",
+    {
         {
-            {"聽從阿爾敏的選擇", 2},
-            {"要求前往地下室", 3},
-            {"要求回到軍團", 4}
-        }
-    };
-*/
+            { // 角色1的選項
+                {"聽從阿爾敏的選擇", 2},
+                {"要求前往地下室", 3}
+            },
+            2  // 角色1有2个选项
+        },
+        // 如果需要，可继续添加其他角色及其选项
+    },
+    1  // 这个节点有1个角色
+};
+
 
 void displayStoryNode(StoryNode node) {
     printf("故事: %s\n", node.story);
