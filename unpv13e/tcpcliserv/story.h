7f32d32c0b8588260e3ef85a17ef84f824d002a3
HungDino <43983154+hungdino@users.noreply.github.com>
2023-12-17 14:58:16 +0800

    Update story.h
    調整選項的 array 位置

--- unpv13e/tcpcliserv/story.h
+++ unpv13e/tcpcliserv/story.h
@@ -4,11 +4,12 @@
 #define OPTION_DESC_LENGTH 256
 #define STORY_LENGTH 1024
 #define ID_LENGTH 32
+#define MAX_NODE 14
 
 int decideNextNode(int currentNode, int numOfNodePlayed){
-    int erenChose[] = [1, 2, 1, 2, 2, 1, -1];
-    int mikasaChose[] = [1, 2, 1, 1, -1, 1];
-    int arminChose[] = [1, 2, 2, 1, -1, -1, 2];
+    int erenChose[MAX_NODE] = [1, 2, 1, 2, 2, 1, -1, 1, 2, ]; // 固定選項為 1, 2, 3，初始化（還沒選）為 -1，該選項不能選為 0
+    int mikasaChose[MAX_NODE] = [1, 2, 1, 1, -1, 1];
+    int arminChose[MAX_NODE] = [1, 2, 2, 1, -1, -1, 2];
     
     int nextNode;
 
@@ -34,9 +35,9 @@ typedef struct {
     int     nodeSeriesNum;                                          // 節點編號
     char    story[STORY_LENGTH];                                    // 故事提示
     char    characterArray[MAX_CHARACTERS_PER_NODE][ID_LENGTH];     // 這個節點的選擇順序 e.g. {"Eren", "Mikasa", "Armin"}
-    char    Eren[MAX_OPTIONS_PER_CHARACTER][OPTION_DESC_LENGTH];    // 儲存艾連的選項
-    char    Mikasa[MAX_OPTIONS_PER_CHARACTER][OPTION_DESC_LENGTH];  // 儲存米卡莎的選項
-    char    Armin[MAX_OPTIONS_PER_CHARACTER][OPTION_DESC_LENGTH];   // 儲存阿爾敏的選項
+    char    Eren[MAX_OPTIONS_PER_CHARACTER+1][OPTION_DESC_LENGTH];    // 儲存艾連的選項
+    char    Mikasa[MAX_OPTIONS_PER_CHARACTER+1][OPTION_DESC_LENGTH];  // 儲存米卡莎的選項
+    char    Armin[MAX_OPTIONS_PER_CHARACTER+1][OPTION_DESC_LENGTH];   // 儲存阿爾敏的選項
 } StoryNode;
 
 StoryNode Main_node1 = {
@@ -44,16 +45,19 @@ StoryNode Main_node1 = {
     "在艾連製造出的巨人骨架保護下，米卡莎與阿爾敏總算從托洛斯特區安然無恙的返回，艾連自己從巨人身體中走了出來，並表示他想起了父親曾告訴他所有事情只要回到家裡的地下室就會有所解答。", // story
     {"Eren", "Mikasa", "Armin"}, // characterArray
     {   // 艾連的選項
+        "None",
         "聽從阿爾敏的選擇",
         "要求前往地下室",
         "要求回到軍團"
     },
     {   // 米卡莎的選項
+        "None",
         "聽從阿爾敏的選擇",
         "聽從艾倫的選擇",
         "阿巴阿巴這個不是我的艾倫，砍砍砍"
     },
     {   // 阿爾敏的選項
+        "None",
         "決定前往地下室",
         "真的可以說服駐紮軍團嗎？我們討論一下",
         "表示自己一定可以說服駐紮軍團，回到軍團繼續下一步動作。"
@@ -67,14 +71,17 @@ StoryNode Main_node2 = {
     {   // 艾連的選項
         "None",
         "None",
+        "None",
         "None"
     },
     {   // 米卡莎的選項
         "None",
         "None",
+        "None",
         "None"
     },
     {   // 阿爾敏的選項
+        "None",
         "讓一群人聚集在石頭的另一邊引誘大部分的巨人到角落，另外再派少數精銳保護艾連",
         "讓一群精銳在石頭的另一邊發出大量聲響和快速移動吸引巨人，主要部隊在艾倫周圍當人牆",
         "表示自己一定可以說服駐紮軍團，回到軍團繼續下一步動作。"
