dad11640d37d97b649745c1e96e7bcacb1897d67
Wei-Ting HUNG <wthung.cs07@nycu.edu.tw>
2024-01-10 22:16:05 +0800

    typo

--- unpv13e/tcpcliserv/server.c
+++ unpv13e/tcpcliserv/server.c
@@ -398,7 +398,7 @@ int receive_player_choice_1_to_3(int client_sock){
     int n;
     srand(time(NULL));
     int random = rand() % 3 + 1;
-    n = Readline(client_sock, buffer, BUFFER_SIZE));
+    n = Readline(client_sock, buffer, BUFFER_SIZE);
     if (n < 0) { //errno == EAGAIN || errno == EWOULDBLOCK
         printf("讀取使用者選項超時，使用者已離開，receive_player_choice_1_to_3 回傳 -1 結束遊戲\n");
         return -1;
